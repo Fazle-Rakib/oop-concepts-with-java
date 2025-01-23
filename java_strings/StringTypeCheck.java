58accdf80c0ae5b2d8eccbc456e82aff5bb5714a
Fazle-Rakib <fazle30@student.sust.edu>
2025-01-23 11:26:13 +0600

    Update: replaced hashCode() with identityHashCode() to understand String

--- java_strings/StringTypeCheck.java
+++ java_strings/StringTypeCheck.java
@@ -5,13 +5,11 @@ public class StringTypeCheck {
         String str = "Java is fun!";
         String str1 = "Java is fun!";
         String strWithNew = new String("Java is fun!");
-        String strWithNew1 = new String("Java is fun!");
         System.out.println("String value: " + str);
         System.out.println("Is String a primitive type? No, it is an object.");
-        System.out.println("Memory Address (hash code) str: " + str.hashCode());
-        System.out.println("Memory Address (hash code) str1: " + str1.hashCode());
-        System.out.println("Memory Address (hash code) strWithNew: " + strWithNew.hashCode());
-        System.out.println("Memory Address (hash code) strWithNew1: " + strWithNew1.hashCode());
+        System.out.println("Hash Code derived from memory address of str: " + System.identityHashCode(str));
+        System.out.println("Hash Code derived from memory address of str1: " + System.identityHashCode(str1));
+        System.out.println("Hash Code derived from memory address of strWithNew: " + System.identityHashCode(strWithNew));
     }
 }
 
