aacd04c35023dd0a7ba2234deb66dec2436caa63
Fazle-Rakib <fazle30@student.sust.edu>
2025-11-27 20:47:31 +0600

    In depth with class and objects

--- java_basics/Array.java
+++ java_basics/Array.java
@@ -2,18 +2,25 @@ package java_basics;
 
 public class Array {
     public static void main(String[] args) {
-        int a = 1; // Memory: 101
-        int b = 2; // Memory: 105
+        int a = 100; // Memory: 101
+        int b = 60; // Memory:  133
 //        [100, 60, 89, 50, 65]
 
-        int[] subject1WiseResult = {100, 60, 89, 50, 65};
-        int subject2WiseResult[] = {100, 60, 89, 50, 65};
+        int[] subjectWiseResult1 = {100, 60, 89, 50, 65};
+        int subjectWiseResult2[] = {100, 60, 89, 50, 65};
+
+        int subjectWiseResult3[];
+        subjectWiseResult3 = new int[5];
 
 //        System.out.println(subject1WiseResult[0]);
 //        subject1WiseResult[3] = 60;
 //        System.out.println(subject1WiseResult[3]);
 
-//        for(int mark : subject1WiseResult) {
+//        for(int i = 0; i < 5; i++){
+//            System.out.println(subjectWiseResult3[i]);
+//        }
+
+//        for(int mark : subjectWiseResult3) {
 //            System.out.println(mark);
 //        }
 
@@ -22,9 +29,9 @@ public class Array {
                 {"A2", "B2" , "C2", "D2"}
         };
 
-        for (String[] seat : seats) {
-            for (String num : seat) {
-                System.out.println(num);
+        for (String[] row : seats) {
+            for (String seat : row) {
+                System.out.println(seat);
             }
         }
     }
---
+++ java_basics/BoxDemo.java
@@ -0,0 +1,42 @@
+package java_basics;
+class Box {
+    double width;
+    double height;
+    double depth;
+
+    void boxInitializer(double width, double height, double depth) {
+        this.width = width;
+        this.height = height;
+        this.depth = depth;
+    }
+
+    void volume() {
+        double total_volume = this.width * this.height * this.depth;
+        System.out.println("Volume of box: " + total_volume);
+    }
+}
+public class BoxDemo {
+    public static void main(String[] args) {
+        Box box1 = new Box();
+        Box box2 = new Box();
+
+        box1.boxInitializer(10,20, 10);
+        box2.boxInitializer(5,10, 15);
+//        box1.width = 10;
+//        box1.height = 20;
+//        box1.depth = 10;
+
+//        box2.width = 5;
+//        box2.height = 10;
+//        box2.depth = 15;
+
+        box1.volume();
+        box2.volume();
+
+//        double volume = box1.width * box1.height * box1.depth;
+//        System.out.println("Volume of box1: " + volume);
+
+//        volume = box2.width * box2.height * box2.depth;
+//        System.out.println("Volume of box2: " + volume);
+    }
+}
